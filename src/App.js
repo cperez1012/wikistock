513299f1cb4ce20ff73b094db23e6dc845b289b6
cperez1012 <perezchristian1012@gmail.com>
2021-02-23 23:04:33 -0500

    edited the text underneath the logo

--- src/App.js
+++ src/App.js
@@ -7,17 +7,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          // className="App-link"
+          // href="https://reactjs.org"
+          // target="_blank"
+          // rel="noopener noreferrer"
         >
-          Learn React
+          {/* Learn React */}
         </a>
-        WikiStocks
+        The leading stock information site.
+
       </header>
     </div>
   );
