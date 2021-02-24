c469f9b103489231ee873753c09bad1c287b4df9
cperez1012 <perezchristian1012@gmail.com>
2021-02-23 23:08:35 -0500

    removed <a> tag and moved text to inside <p> tag

--- src/App.js
+++ src/App.js
@@ -7,18 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {/* Edit <code>src/App.js</code> and save to reload. */}
+          The leading stock information site on the net.
         </p>
-        <a
-          // className="App-link"
-          // href="https://reactjs.org"
-          // target="_blank"
-          // rel="noopener noreferrer"
-        >
-          {/* Learn React */}
-        </a>
-        The leading stock information site.
-
       </header>
     </div>
   );
