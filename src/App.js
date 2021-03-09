fe8018366617e5700428c72da1aca7e25ab45295
cperez1012 <perezchristian1012@gmail.com>
2021-03-09 15:12:36 -0500

    commented out Route and Stock component import lines, trouble shooting route path

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import logo from './WikiStock.png';
 import './App.css';
+import React from 'react';
+// import { Route } from 'react-router-dom';
+// import Stocks from './components/Stocks.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          The leading stock information site on the net.
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  render (){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+
+          <p>
+            The leading stock information site on the web.
+          </p>
+        </header>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
